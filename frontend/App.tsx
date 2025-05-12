0cab4f7b9feda6a818c3c3d6ab6858c6914b6a17
Pau1R <rudnikov.paul@yandex.ru>
2025-05-13 01:52:41 +0500

    testing if file functions

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './src/pages/HomePage'
-import CreateVotePage from './src/pages/CreateVotePage'
-import VotePage from './src/pages/VotePage'
-import ResultsPage from './src/pages/ResultsPage'
-import PublishPage from './src/pages/PublishPage'
-import './src/App.css'
+// import { Routes, Route } from 'react-router-dom'
+// import HomePage from './src/pages/HomePage'
+// import CreateVotePage from './src/pages/CreateVotePage'
+// import VotePage from './src/pages/VotePage'
+// import ResultsPage from './src/pages/ResultsPage'
+// import PublishPage from './src/pages/PublishPage'
+// import './src/App.css'
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/create" element={<CreateVotePage />} />
+//       <Route path="/vote/:code" element={<VotePage />} />
+//       <Route path="/results/:code" element={<ResultsPage />} />
+//       <Route path="/publish" element={<PublishPage />} />
+//     </Routes>
+//   )
+// }
+
+// export default App
+
+
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreateVotePage />} />
-      <Route path="/vote/:code" element={<VotePage />} />
-      <Route path="/results/:code" element={<ResultsPage />} />
-      <Route path="/publish" element={<PublishPage />} />
-    </Routes>
-  )
+  return <div>Hello</div>;
 }
-
-export default App
+export default App;
